11a106a4b578606fee580014077bee0fb9859721
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-08-21 23:47:54 +0700

    feat: coding html

---
+++ src/view/layout/header.js
@@ -0,0 +1,23 @@
+import { IcLogo, IcArrowRight } from '../../resources/assets/icons/index.js';
+import navChild from '../components/navChild.js';
+
+const listNav = ['Movies', 'TV Shows'];
+const listNavIcon = [`<figure> <img src="${IcArrowRight}" alt="Logout"> <figcaption>Logout</figcaption> </figure>`];
+
+const header = () => {
+  return `
+  <div class="header">
+      <div class="header--logo">
+          <figure>
+              <img src="${IcLogo}" alt="logo">
+          </figure>
+      </div>
+      <div class="header--nav">
+         <ul> ${navChild(listNav)} </ul>
+         <ul>  ${navChild(listNavIcon)} </ul>
+      </div>
+  </div>
+  `;
+};
+
+export default header;
