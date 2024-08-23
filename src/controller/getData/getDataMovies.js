84d1722acec3b51bb9379f794b3babbc7614d929
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-08-24 01:58:25 +0700

    feat: handle data movies #2

---
+++ src/controller/getData/getDataMovies.js
@@ -0,0 +1,56 @@
+import { getAllMovies } from '../../services/movie.services';
+
+const listALLMovies = () => {
+    return listmovies = [
+    {
+      nameMovie: 'Black Widow',
+      avtMovie: 'https://s3.cloudfly.vn/practice-js/images/black-widow.png',
+      iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+      quantityStar: 6.9,
+    },
+    {
+        nameMovie: 'Shang Chi',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/sang-chi.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 7.9,
+      },
+      {
+        nameMovie: 'Loki',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/loki.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 8.4,
+      },
+      {
+        nameMovie: 'How I Met Your Mother',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/your-mother.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 8.3,
+      },
+      {
+        nameMovie: 'Money Heist',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/Money-heist.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 8.3,
+      },
+      {
+        nameMovie: 'Friends',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/friend.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 8.8,
+      },
+      {
+        nameMovie: 'The Big Bang Theory',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/BigBang.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 8.1,
+      },
+      {
+        nameMovie: 'Two And a Half Men',
+        avtMovie: 'https://s3.cloudfly.vn/practice-js/images/two-man.png',
+        iconStar: 'https://s3.cloudfly.vn/practice-js/images/ic-star.svg',
+        quantityStar: 7,
+      },
+  ]; 
+};
+
+export default listALLMovies;
