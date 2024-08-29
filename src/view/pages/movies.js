25e91350e1e99cdf70966b5346c8ad8126f88dd3
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-08-29 23:28:04 +0700

    feat: completed style movies

---
+++ src/view/pages/movies.js
@@ -0,0 +1,27 @@
+import header from '../layout/header';
+import footer from '../layout/footer';
+
+const homePage = () => {
+  return `
+    ${header()}
+    <section class ="section-main">
+     <div class="section-main--title">
+       <h3>MaileHereko</h3>
+     </div> <!-- end section-main--title --> 
+     <div class="section-main--desc">
+      <p>Movies</p>
+     </div> <!-- end section-main--desc--> 
+     <div class ="section-main--search">
+      <div class="search-container">
+       <input class="search-container--input" type="text" placeholder="Search Movies or TV Shows">
+       <img class="search-container--icon" src="https://s3.cloudfly.vn/practice-js/images/search-normal.svg" alt="icon search">
+      </div> 
+     </div> <!-- end section-main--search-->
+      <p class="section-main--desc-subNav">120 items</span></p>
+      <div class="section-main--list-movies"></div>
+    </section>
+    ${footer()}
+    `;
+};
+
+export default homePage ;
