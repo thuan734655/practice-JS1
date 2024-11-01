d32458a6e65b12adad9b716cd5f895bafafa412c
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-11-01 21:38:40 +0700

    change baseURl

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://practice-js-server.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
